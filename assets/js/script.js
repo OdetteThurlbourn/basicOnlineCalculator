df7f49e16d7541f15e63c448a4e6a19e73a3b8fa
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2022-11-28 16:06:08 +0000

    Define  the const, define the function (parseFloat). Basic calculating formual for our variables and operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,33 @@
  *  a const variable - note const variables cannot be reassigned 
 */
 
-const operator = document.querySelectorAll("operator");
-const number = document.querySelectorAll("number");
+const operators = document.querySelectorAll("operator");
+const numbers = document.querySelectorAll("number");
+const display = document.querySelector("display");
 const equal = document.querySelector("equal");
-const reset = document.querySelector("reset");
-const result = document.querySelector("result");
+const allClear = document.querySelector("allClear");
+const backSpace = document.querySelector("backSpace");
+const enter = document.getElementById("equals")
+const negativePositive = document.querySelector("negativePositive")
+const digits = display.innerText.length;
 
+/**  Using the parseFloat function 
+* to define the operators and to return the first number after the value
+* has been parsed as a string
+*/
+
+function add(a, b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a, b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
+function divide(a, b) {
+    return parseFloat(a) / parseFloat(b);
+}
+
+function multiply(a, b) {
+    return parseFloat(a) * parseFloat(b);
+}
