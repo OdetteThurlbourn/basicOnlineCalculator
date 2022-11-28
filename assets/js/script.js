54ef9f11f53c62f96b351b47b84586c416de8290
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2022-11-28 15:46:30 +0000

    Declare the const variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/** Assign a values to the operations, numbers etc. using
+ *  a const variable - note const variables cannot be reassigned 
+*/
+
+const operator = document.querySelectorAll("operator");
+const number = document.querySelectorAll("number");
+const equal = document.querySelector("equal");
+const reset = document.querySelector("reset");
+const result = document.querySelector("result");
+
